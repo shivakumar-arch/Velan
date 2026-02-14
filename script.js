55c2ba6a75eb9cfc4c90875713b8e8778d1d29c4
shivakumar-arch <shivakumar10officially@gmail.com>
2026-02-14 09:35:16 +0530

    Updated to Giphy links and high-res falling 3D PNGs

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Realistic 3D PNG Links
 const pngs = {
     rose: "https://img.icons8.com/fluency/96/rose.png",
     ring: "https://img.icons8.com/fluency/96/diamond-ring.png",
@@ -10,13 +9,13 @@ const pngs = {
 };
 
 const stories = [
-    { gif: "https://media1.tenor.com/m/lM5zS9-rFp4AAAAC/bubu-dudu-bubu.gif", p: "rose", title: "Rose Day", text: "chellaameee, you are the most beautiful flower in my universe." },
-    { gif: "https://media1.tenor.com/m/A65_bB1EtiMAAAAC/peach-cat-propose.gif", p: "ring", title: "Propose Day", text: "thangameee, I want to spend forever with you." },
-    { gif: "https://media1.tenor.com/m/Jb9kGfK4nLEAAAAC/bubu-dudu-bubu.gif", p: "choco", title: "Chocolate Day", text: "vairameeeeeee, you are sweeter than any chocolate." },
-    { gif: "https://media1.tenor.com/m/8QGzQ21mJg4AAAAC/tkthao219-bubududu.gif", p: "teddy", title: "Teddy Day", text: "pondatiiiiiiiiiiii, you are my ultimate comfort." },
-    { gif: "https://media1.tenor.com/m/39hP_IqW2J0AAAAC/pink-and.gif", p: "promise", title: "Promise Day", text: "azhagiiiiiiiiiiiiiii, I promise to hold your hand forever." },
-    { gif: "https://media1.tenor.com/m/Ztw1z9dF8D4AAAAC/bubu-dudu-bubu.gif", p: "hug", title: "Hug Day", text: "baby gurllllllllll, un kooda irukardhu thaan enaku happiness." },
-    { gif: "https://media1.tenor.com/m/vH9ZJ4n0pU0AAAAC/tkthao219-bubududu.gif", p: "kiss", title: "Kiss Day", text: "milkcake ehhhhh, one kiss from you makes everything better." }
+    { gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb200aHl5bnF4OG44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44/kGj2uE1nF70YjR1nLq/giphy.gif", p: "rose", title: "Rose Day", text: "chellaameee, you are the most beautiful flower in my universe." },
+    { gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb200aHl5bnF4OG44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44/l4pTdcifPGuRoGnRu/giphy.gif", p: "ring", title: "Propose Day", text: "thangameee, I want to spend forever with you." },
+    { gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb200aHl5bnF4OG44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44/Q4sEE1X5HqCJO/giphy.gif", p: "choco", title: "Chocolate Day", text: "vairameeeeeee, you are sweeter than any chocolate." },
+    { gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb200aHl5bnF4OG44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44/L12g7V0Jfk2iM5Cwus/giphy.gif", p: "teddy", title: "Teddy Day", text: "pondatiiiiiiiiiiii, you are my ultimate comfort." },
+    { gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb200aHl5bnF4OG44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44/1k3RVbvmFvFsc/giphy.gif", p: "promise", title: "Promise Day", text: "azhagiiiiiiiiiiiiiii, I promise to hold your hand forever." },
+    { gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb200aHl5bnF4OG44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44/3oEdv4hwWTzBhWvaU0/giphy.gif", p: "hug", title: "Hug Day", text: "baby gurllllllllll, un kooda irukardhu thaan enaku happiness." },
+    { gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb200aHl5bnF4OG44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44/117o9DtK0h5XgY/giphy.gif", p: "kiss", title: "Kiss Day", text: "milkcake ehhhhh, one kiss from you makes everything better." }
 ];
 
 let currentStep = -1;
@@ -34,8 +33,8 @@ function nextStory() {
     if (currentStep < stories.length) currentPNG = stories[currentStep].p;
 
     // Trigger Screen Fill with PNGs
-    for(let i = 0; i < 40; i++) {
-        setTimeout(() => spawnPNG(pngs[currentPNG]), i * 40);
+    for(let i = 0; i < 45; i++) {
+        setTimeout(() => spawnPNG(pngs[currentPNG]), i * 45);
     }
 
     setTimeout(() => {
@@ -58,14 +57,14 @@ function spawnPNG(url) {
     let startX = (Math.random() * 100) + "vw";
     img.style.setProperty('--startX', startX);
     img.style.left = startX;
-    img.style.width = (Math.random() * 40 + 60) + "px";
+    img.style.width = (Math.random() * 40 + 70) + "px";
     img.style.animationDuration = (Math.random() * 2 + 2) + "s";
     fxLayer.appendChild(img);
     setTimeout(() => img.remove(), 4000);
 }
 
 function loadProposal() {
-    mainGif.src = "https://media1.tenor.com/m/gUiu1zyxfzYAAAAC/bear-kiss-bear-kisses.gif";
+    mainGif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb200aHl5bnF4OG44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44/26BRv0ThflsHCqDrG/giphy.gif";
     document.getElementById("titleText").innerText = "My Pondati...";
     document.getElementById("msgText").innerText = "Will you be my Valentine forever?";
     document.getElementById("actionArea").innerHTML = `
@@ -78,9 +77,9 @@ function loadProposal() {
 
 function showFinal() {
     inkContent.classList.add("hidden");
-    for(let i = 0; i < 60; i++) setTimeout(() => spawnPNG(pngs.kiss), i * 30);
+    for(let i = 0; i < 65; i++) setTimeout(() => spawnPNG(pngs.kiss), i * 35);
     setTimeout(() => {
-        mainGif.src = "https://media1.tenor.com/m/IVKKi1R5uc4AAAAC/goma-happy.gif";
+        mainGif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb200aHl5bnF4OG44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44aW44/11s7Ke7jcNxCHS/giphy.gif";
         document.getElementById("titleText").innerText = "Yayyy! 🎉";
         document.getElementById("msgText").innerText = "Happy Valentine's Day! Love you 3000! ❤️";
         document.getElementById("actionArea").innerHTML = "";
